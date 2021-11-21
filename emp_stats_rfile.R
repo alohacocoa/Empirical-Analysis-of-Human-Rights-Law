468f30345f4c67ef83d5309693ff370e6e9b11c5
dave <david.klug03@gmail.com>
2021-11-21 16:10:15 +0100

    Revert "Created "violations" and "violations_with_text" variables"
    This reverts commit 61a3eafb36b3bfa33db773fe619a15e8a9d7ba03.

--- emp_stats_rfile.R
+++ emp_stats_rfile.R
@@ -1,14 +1,4 @@
 
-#this clears the environment
-rm(list = ls())
-
-#this sets the wd to the directory of this R file. might not be necessary, but better be safe than sorry
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-#sets R to english because I don't like german warnings
-Sys.setenv(LANG = "en")
-
-
 library(haven)
 library(tidyverse)
 library(margins)
@@ -51,57 +41,21 @@ hudoc$country <- gsub("suisse", "switzerland", hudoc$country)
 #hudoc$text <- str_extract(hudoc$conclusion, "^\\D+")
 #hudoc$text <- rm()
 hudoc$text <- hudoc$conclusion
-
-
+#hudoc$article <- str_first_number(hudoc$conclusion)
 removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9 ]", " ", x)
 hudoc$text <- sapply(hudoc$text, removeSpecialChars)
 
-#this makes it so that the french is the same as english
+#junk
 hudoc$text <- gsub("  |   ", " ", hudoc$text)
 hudoc$text <- gsub(" de l ", " of ", hudoc$text)
 hudoc$text <- gsub("article ", "art ", hudoc$text)
 hudoc$text <- gsub("non ", "no ", hudoc$text)
-
-
-#this looks for the phrase "violation of" followed by any number of non-digit characters and ends with one or more digits. the whole phrase must not be preceded by "no ". the matches are then written into the new varialbe "violations_with_text". the variable is a list
-hudoc$violations_with_text <-  stringr::str_extract_all(hudoc$text, "(?<!no\\s)violation\\sof\\D{0,20}\\d+") 
-
-
-#this creates a new variable called "violations". it gets rid of the text and leaves only the numbers. I couldn't do it with only regex because for some reason, using negative and positive lookbehind does not work (in R). anyway, the variable is also a list
-for (l in 1:length(hudoc$violations_with_text)) {
-  hudoc$violations[l] <- stringr::str_extract_all(hudoc$violations_with_text[l], "\\d+")
-} 
-
-
-#still to do before data visualization:
-# - find and get rid of duplicates
-# - use hudoc$violations variable to create a new row in every case where there is more than one violation, copying everything except for the value of hudoc$violation. don't know how to do that yet
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#junk
-# hudoc$text <- gsub("preliminary objection joined to merits and dismissed victim remainder inadmissible violation of |preliminary objection joined to merits and dismissed victim violation of |preliminary objection joined to merits and dismissed ","preliminary objection joined to merits and dismissed ", hudoc$text)
-# hudoc$text <- gsub("exception pr liminaire rejet e |exceptions pr liminaires rejet es |preliminary objection dismissed ", "preliminary objections dismissed ", hudoc$text)
-# hudoc$text <- gsub("partiellement irrecevable ", "partially irrecevable ", hudoc$text)
-# hudoc$text <- gsub("remainder inadmissible violation  |remainder inadmissible struck out the list  ", "remainder inadmissible  ", hudoc$text)
-# hudoc$text <- gsub("preliminary objection joined to merits and dismissed victim remainder inadmissible |preliminary objection joined to merits and dismissed victim violation ", "preliminary objection joined to merits and dismissed ", hudoc$text)
-# hudoc$text <- gsub("partially irrecevable violation", "partially irrecevable ", hudoc$text)
+hudoc$text <- gsub("preliminary objection joined to merits and dismissed victim remainder inadmissible violation of |preliminary objection joined to merits and dismissed victim violation of |preliminary objection joined to merits and dismissed ","preliminary objection joined to merits and dismissed ", hudoc$text)
+hudoc$text <- gsub("exception pr liminaire rejet e |exceptions pr liminaires rejet es |preliminary objection dismissed ", "preliminary objections dismissed ", hudoc$text)
+hudoc$text <- gsub("partiellement irrecevable ", "partially irrecevable ", hudoc$text)
+hudoc$text <- gsub("remainder inadmissible violation  |remainder inadmissible struck out the list  ", "remainder inadmissible  ", hudoc$text)
+hudoc$text <- gsub("preliminary objection joined to merits and dismissed victim remainder inadmissible |preliminary objection joined to merits and dismissed victim violation ", "preliminary objection joined to merits and dismissed ", hudoc$text)
+hudoc$text <- gsub("partially irrecevable violation", "partially irrecevable ", hudoc$text)
 
 #merge text and article no.
 #hudoc$violation_art <- paste(hudoc$text, hudoc$article, sep = "art ")
