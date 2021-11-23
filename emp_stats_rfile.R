4b6c560f410db9df8882c466e97011751ccd578b
isimiha <93491989+isimiha@users.noreply.github.com>
2021-11-23 14:26:51 +0100

    Update emp_stats_rfile.R

--- emp_stats_rfile.R
+++ emp_stats_rfile.R
@@ -33,8 +33,6 @@ hudoc <- hudoc %>% rename(case = Document.Title  ,case_id = Application.Number,
                           concdate = Date, 
                           conclusion = Conclusion)%>% select(-3, -4) %>% distinct(case_id, .keep_all = TRUE)
 
-#Recode and create variables
-
 #convert everything to lower case to facilitate recoding
 hudoc$conclusion <- sapply(hudoc$conclusion, tolower)
 hudoc$case <- sapply(hudoc$case, tolower)
@@ -46,15 +44,8 @@ hudoc$country <- word(hudoc$country, 1)
 table(hudoc$country)
 hudoc$country <- gsub("suisse", "switzerland", hudoc$country)
 
-#create violation variable (first filter for the text and number of article separately, then merge together. Afterwards remove junk/summarize)
-#create the text variable for the extraction of violations
-#hudoc$text <- str_extract(hudoc$conclusion, "^\\D+")
-#hudoc$text <- rm()
+#create text variable as a first step to retract violated articles
 hudoc$text <- hudoc$conclusion
-
-
-#removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9\\+ ]", " ", x)
-#hudoc$text <- sapply(hudoc$text, removeSpecialChars)
 
 #this makes it so that the french is the same as english
 hudoc$text <- gsub("  |   ", " ", hudoc$text)
@@ -67,32 +58,14 @@ hudoc$text <- gsub("\\+", " violation of art. ", hudoc$text)
 #this looks for the phrase "violation of" followed by any number of non-digit characters and ends with one or more digits. the whole phrase must not be preceded by "no ". the matches are then written into the new varialbe "violations_with_text". the variable is a list
 hudoc$violations_with_text <-  stringr::str_extract_all(hudoc$text, "(?<!no\\s)violation\\sof\\sart\\D{0,20}\\d+(?!\\sof\\sprotocol)") 
 
-
-# #this creates a new variable called "violations_with_text". it gets rid of the text and leaves only the numbers. I couldn't do it with only regex because for some reason, using negative and positive lookbehind does not work (in R). anyway, the variable is also a list
-# for (l in 1:length(hudoc$violations_with_text)) {
-#   hudoc$violations_with_text_numbers[l] <- stringr::str_extract_all(hudoc$violations_with_text[l], "\\d+")
-# } 
-
 #this loop below gets rid of duplicate values from situations like "violation of art. 14+3" where 14 was matched twice
 for (l in 1:length(hudoc$violations_with_text)) {
   hudoc$violations_with_text[[l]] <- unique(hudoc$violations_with_text[[l]])
 }
 
 #this gets rid of rows where "hudoc$violations_with_text" is missing values
-hudoc <- hudoc[!is.na(hudoc$violations_with_text), ]
-
-
-
-
-
-
-
-
-
-#still to do before data visualization:
-# - find and get rid of duplicates (is included in the first lines of code)
-# - use hudoc$violations variable to create a new row in every case where there is more than one violation, copying everything except for the value of hudoc$violation. don't know how to do that yet
-# => I was a bit lazy and chose another approach, I groupd the violations (violations_with_text) by country, aggregated all violations per country, got rid of the "violations of" part, and extracted the numbers
+#hudoc <- hudoc[!is.na(hudoc$violations_with_text), ]
+#hudoc <- list.clean(hudoc, fun = function(x) length(x) == 0L, recursive = TRUE)
 
 #create new dataobject and group new dataset "bycountry" by country, remove list values by applying unlist
 bycountry <- apply(hudoc, 2, function(y) sapply(y, function(x) paste(unlist(x), collapse=" ")))
@@ -128,46 +101,6 @@ switz
 
 #Interestingly, art 6 is the most violated article for Switzerland 
 table(switz)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#junk
-# hudoc$text <- gsub("preliminary objection joined to merits and dismissed victim remainder inadmissible violation of |preliminary objection joined to merits and dismissed victim violation of |preliminary objection joined to merits and dismissed ","preliminary objection joined to merits and dismissed ", hudoc$text)
-# hudoc$text <- gsub("exception pr liminaire rejet e |exceptions pr liminaires rejet es |preliminary objection dismissed ", "preliminary objections dismissed ", hudoc$text)
-# hudoc$text <- gsub("partiellement irrecevable ", "partially irrecevable ", hudoc$text)
-# hudoc$text <- gsub("remainder inadmissible violation  |remainder inadmissible struck out the list  ", "remainder inadmissible  ", hudoc$text)
-# hudoc$text <- gsub("preliminary objection joined to merits and dismissed victim remainder inadmissible |preliminary objection joined to merits and dismissed victim violation ", "preliminary objection joined to merits and dismissed ", hudoc$text)
-# hudoc$text <- gsub("partially irrecevable violation", "partially irrecevable ", hudoc$text)
-
-#merge text and article no.
-#hudoc$violation_art <- paste(hudoc$text, hudoc$article, sep = "art ")
-
-#create categorical variable
-#hudoc$catvar <- as.character(as.numeric(as.factor(hudoc$text)))
-#table(hudoc$catvar)
-#hudoc$catvar <- as.factor(ifelse(hudoc$catvar == 9, 1, 0))
-
-#create dummy variable
-#hudoc$dummy <- as.character(as.numeric(as.factor(hudoc$text)))
-#hudoc$dummy <- car::recode(hudoc$dummy, "1= '0'; 9 = '1'; else = NA")
-
-
 
 
 
