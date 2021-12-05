f5062f634b3ad117da89dc41f0987af90993862a
dave <david.klug03@gmail.com>
2021-12-05 19:29:03 +0100

    Finalized R file

--- emp_stats_rfile.R
+++ emp_stats_rfile.R
@@ -1,3 +1,6 @@
+# Preparing the environment ----
+#==========================#
+
 
 #this clears the environment
 rm(list = ls())
@@ -5,27 +8,22 @@ rm(list = ls())
 #this sets the wd to the directory of this R file. might not be necessary, but better be safe than sorry
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-#sets R to english because I don't like german warnings
+#sets R to English
 Sys.setenv(LANG = "en")
 
-
+#this loads packages
 library(haven)
 library(tidyverse)
-library(margins)
 library(magrittr)
-library(ggeffects)
 library(ggtext)
-library(lme4)
-library(splines)
-library(stargazer)
-library(dplyr)
-library(tidytext)
 library(strex)
-library(tm)
-library(stringr)
 
 
-## Data Preparation
+
+# CSV import ----
+#==========================#
+
+
 
 #Import the dataset. Filter for variables that we need in the end, rename the variables to facilitate subsequent coding, and remove duplicates.
 hudoc <- read.csv("hudoc.csv")
@@ -34,7 +32,15 @@ hudoc <- hudoc %>% rename(case = Document.Title  ,case_id = Application.Number,
                           concdate = Date, 
                           conclusion = Conclusion)%>% select(-3, -4) %>% distinct(case_id, .keep_all = TRUE)
 
-#convert everything to lower case to facilitate recoding
+
+
+
+# Data cleaning and variable extraction ----
+#==========================#
+
+
+
+#convert everything to lowercase to facilitate recoding
 hudoc$conclusion <- sapply(hudoc$conclusion, tolower)
 hudoc$case <- sapply(hudoc$case, tolower)
 
@@ -48,7 +54,7 @@ hudoc$country <- gsub("suisse", "switzerland", hudoc$country)
 #create text variable as a first step to retract violated articles
 hudoc$text <- hudoc$conclusion
 
-#this makes it so that the french is the same as english
+#this makes it so that the French is the same as English
 hudoc$text <- gsub("  |   ", " ", hudoc$text)
 hudoc$text <- gsub(" de l'", " of ", hudoc$text)
 hudoc$text <- gsub("article ", "art. ", hudoc$text)
@@ -56,13 +62,19 @@ hudoc$text <- gsub("non ", "no ", hudoc$text)
 hudoc$text <- gsub("non-violation", "no violation", hudoc$text)
 hudoc$text <- gsub("\\+", " violation of art. ", hudoc$text)
 
-#this looks for the phrase "violation of" followed by any number of non-digit characters and ends with one or more digits. the whole phrase must not be preceded by "no ". the matches are then written into the new varialbe "violations_with_text". the variable is a list
+#this looks for the phrase "violation of" followed by any number of non-digit characters and ends with one or more digits. the whole phrase must not be preceded by "no ". the matches are then written into the new variable "violations_with_text". the variable is a list. protocols are excluded
 hudoc$violations_with_text <-  stringr::str_extract_all(hudoc$text, "(?<!no\\s)violation\\sof\\sart\\D{0,20}\\d+(?!\\sof\\sprotocol)") 
 
 #this loop below gets rid of duplicate values from situations like "violation of art. 14+3" where 14 was matched twice
 for (l in 1:length(hudoc$violations_with_text)) {
   hudoc$violations_with_text[[l]] <- unique(hudoc$violations_with_text[[l]])
 }
+
+
+
+# Creating new data objects for analysis ----
+#==========================#
+
 
 #create new dataobject and group new dataset "bycountry" by country, remove list values by applying unlist
 bycountry <- apply(hudoc, 2, function(y) sapply(y, function(x) paste(unlist(x), collapse=" ")))
@@ -71,7 +83,7 @@ bycountry <- apply(hudoc, 2, function(y) sapply(y, function(x) paste(unlist(x), 
 agg <- aggregate(data=bycountry, violations_with_text~country, paste0, collapse=' ')
 
 
-#get number of violations for slovakia (since the values are displayed as a list, we need to apply unlist; in a second step we remove the spaces)
+#get number of violations for Slovakia (since the values are displayed as a list, we need to apply unlist; in a second step we remove the spaces)
 slovakia <- agg$violations_with_text[1]
 slovakia <- str_split(slovakia, 'violation of art. ')
 slovakia <- unlist(slovakia)
@@ -81,7 +93,7 @@ slovakia
 table(slovakia)
 
 
-#get number of violations for switzerland (since the values are displayed as a list, we need to apply unlist; in a second step we remove the spaces)
+#get number of violations for Switzerland (since the values are displayed as a list, we need to apply unlist; in a second step we remove the spaces)
 switz <- agg$violations_with_text[2]
 switz <- str_split(switz, 'violation of art. ')
 switz <- unlist(switz)
@@ -115,6 +127,11 @@ comb.table <- rbind(slov.table, switz.table) %>%
   mutate(total_country = sum(count), proportion = count/total_country) %>%
   ungroup()
 comb.table$violations <- factor(comb.table$violations, ordered = TRUE, c("", 2, 3, 5, 6, 8, 10, 13, 14, 34)) #for some reason "" was listed as a level too
+
+
+
+# Plots ----
+#==========================#
 
 
 #plotting both countries separately
